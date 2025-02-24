c33fb84c06a5bcc8256a1845e96ecc56b474b2d7
I.Maksat <maksat@MacBook-Air-Maksat.local>
2025-02-24 12:22:08 +0600

    Method for launching LibraryServer on port 9889

--- src/kg/attractor/java/Main.java
+++ src/kg/attractor/java/Main.java
@@ -1,7 +1,14 @@
 package kg.attractor.java;
 
+import kg.attractor.java.server.LibraryServer;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            new LibraryServer("localhost", 9889).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
