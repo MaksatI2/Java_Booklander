956762746ea23d5adfa99c990db78051d22d48b6
I.Maksat <maksat@MacBook-Air-Maksat.local>
2025-02-24 12:04:29 +0600

    created all the necessary fields for the employee: id — unique employee ID. name - name. borrowedBooks — list of book IDs that the employee borrowed.

--- src/kg/attractor/java/model/Employee.java
+++ src/kg/attractor/java/model/Employee.java
@@ -1,4 +1,21 @@
 package kg.attractor.java.model;
 
+import java.util.List;
+
 public class Employee {
+    private String id;
+    private String name;
+    private List<String> borrowedBooks;
+
+    public Employee(String id, String name, List<String> borrowedBooks) {
+        this.id = id;
+        this.name = name;
+        this.borrowedBooks = borrowedBooks;
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+
+    public List<String> getBorrowedBooks() { return borrowedBooks; }
 }
+
