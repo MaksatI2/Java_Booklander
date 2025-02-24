61965440813072e3ed1b3662f5370d60ec5c15bd
I.Maksat <maksat@MacBook-Air-Maksat.local>
2025-02-24 11:59:38 +0600

    created all the required fields: id - unique identifier. title — the title of the book. author - author. issued — whether the book has been issued. borrowerId — ID of the employee who borrowed the book. image — path to the cover image.

--- src/kg/attractor/java/model/Book.java
+++ src/kg/attractor/java/model/Book.java
@@ -1,4 +1,43 @@
 package kg.attractor.java.model;
 
 public class Book {
+    private String id;
+    private String title;
+    private String author;
+    private boolean issued;
+    private String borrowerId;
+    private String image;
+
+    public Book(String id, String title, String author, boolean issued) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.issued = issued;
+    }
+
+
+    public String getBorrowerId() {
+        return borrowerId;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isIssued() {
+        return issued;
+    }
 }
+
